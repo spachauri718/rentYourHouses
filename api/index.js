85b7f933aa3c5c87fe0638dcf577395a77ba25d7
spachauri718 <spachauri718@gmail.com>
2023-12-16 13:50:14 +0530

    add api

---
+++ api/index.js
@@ -0,0 +1,257 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const Place = require("./models/Place");
+const Booking = require("./models/Booking");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
+const multer = require("multer");
+const fs = require("fs"); //for renaming the files 
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "ajsdofjakjoajsljoajfokjoo0i-weq9r0m";
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+mongoose.connect("mongodb+srv://dracooo:dracooo123@cluster0.yqym3ph.mongodb.net/");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error  "));
+db.once("open", function () {
+  console.log("db connnected");
+});
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+//req.body se name email aur password extract kar liya jo ki form submit karne ke baad kar sakte h fir ek user create kiya jiska password encrypt kiya 
+
+app.post("/register", async (req, res) => {
+
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+});
+
+
+//login page mein bhi ek form h to req.body se email and password jo bhi user ne submit kiya h wo extract kya fir email ko find kiya ki user exist karta h ya nhi then agar user exist karta h to password check kiya and ek cookie sign kar li jismein id and password hoga jisse ham user ka name show kara sakein 
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        {
+          email: userDoc.email,
+          id: userDoc._id,
+        },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(userDoc);
+        }
+      );
+    } else {
+      res.status(422).json("pass not ok");
+    }
+  } else {
+    res.json("not found");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+
+  //image downloader package (can refer to the doc) dest mein link se download ho jayengi dest ek link ho jayegi jismein image download ho jayegi
+
+console.log({__dirname});
+
+app.post("/upload-by-link", async (req, res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + '/uploads/' +newName,
+  });
+  res.json(newName);
+});
+
+
+const photosMiddleware = multer({dest:'uploads/'});
+
+app.post("/upload" , photosMiddleware.array('photos',100), (req,res) => {
+
+  const uploadedFiles = [];
+
+  for(let i = 0; i <req.files.length; i++) {
+    const {path, originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const extension = parts[parts.length - 1];
+    const newPath = path+ '.' + extension;
+    fs.renameSync(path, newPath)
+    uploadedFiles.push(newPath.replace('uploads\\',''));
+  }
+  res.json(uploadedFiles);
+
+})
+
+
+
+ app.post('/places' , (req, res) => {
+  const {token} = req.cookies;
+  const { title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,price
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+    owner:userData.id,
+     title,
+    address,
+    photos : addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+    });
+     res.json(placeDoc);
+  });
+});
+
+
+app.get('/user-places' ,async (req, res) => {
+  const {token} = req.cookies;  
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const {id} = userData;
+    res.json( await Place.find({owner:id}) );
+  });
+});
+
+
+
+app.get('/places/:id', async (req,res) => {
+  const {id} = req.params;
+  res.json(await Place.findById(id));
+});
+
+app.put('/places/', async (req,res) => {
+  const {token} = req.cookies;
+  const {id, title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if(err)throw err;
+    const placeDoc = await Place.findById(id);
+    if(userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+      title,
+      address,
+      photos : addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
+      })
+     await placeDoc.save();
+      res.json('ok')
+    }
+  })
+})
+
+app.get('/places',async (req, res) => {
+  res.json(await Place.find());
+})
+
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
+
+app.post('/bookings', async (req, res) => {
+  const userData = await getUserDataFromReq(req);
+  const {
+    place,checkIn,checkOut,numberOfGuests,name,phone,price,
+  } = req.body;
+  Booking.create({
+    place,checkIn,checkOut,numberOfGuests,name,phone,price,
+    user:userData.id,
+  }).then((doc) => {
+    res.json(doc);
+  }).catch((err) => {
+    throw err;
+  });
+});
+
+app.get('/bookings',async (req, res)=>{
+    const userData = await getUserDataFromReq(req)
+    res.json(await Booking.find({user:userData.id}))
+  })
+
+app.listen("3000", () => {
+  console.log("listening on 3000");
+});
